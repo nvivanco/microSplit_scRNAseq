c49baeef3812b6d7a260f8fb2159c2964b267561
Khoa Hoang <khoang99@stanford.edu>
2025-09-13 00:29:21 -0700

    fix paths in setup script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ export ECOLI_REF_GENOME="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_gen
 export ECOLI_GTF="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_genomic.gtf"
 export ECOLI_REF_TRNX="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_cds_from_genomic.fna"
 
-export BSUB_REF_GENOME="$RAW_DATA/references/bsubtilis/GCF_000009045.1_ASM904v1_genomic.gtf"
+export BSUB_REF_GENOME="$RAW_DATA/references/bsubtilis/GCF_000009045.1_ASM904v1_genomic.fna"
 export BSUB_GTF="$RAW_DATA/references/bsubtilis/GCF_000009045.1_ASM904v1_genomic.gtf"
 export BSUB_REF_TRNX="$RAW_DATA/references/bsubtilis/GCF_000005845.GCF_000009045.1_ASM904v1_cds_from_genomic.fna"
 
