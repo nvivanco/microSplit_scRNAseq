222850084dc5056688c9c3a80791726691c1a857
Khoa Hoang <khoang99@stanford.edu>
2025-09-17 11:06:36 -0700

    minor typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # default paths
-export RAW_DATA="$OAK/scRNAseq/raw_data/"
-export PROCESSED_DATA="$OAK/scRNAseq/processed_data/"
+export RAW_DATA="$OAK/scRNAseq/raw_data"
+export PROCESSED_DATA="$OAK/scRNAseq/processed_data"
 export ECOLI_REF_GENOME="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_genomic.fna"
 export ECOLI_GTF="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_genomic.gtf"
 export ECOLI_REF_TRNX="$RAW_DATA/references/ecoli/GCF_000005845.2_ASM584v2_cds_from_genomic.fna"
